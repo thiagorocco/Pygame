9803f6d5c684eb7eddada273d29065fa66d0e582
thiago-rocco <deroccotr@gmail.com>
2023-08-29 20:23:23 -0300

    criando linhas no pygame com uma classe Linha

--- 0-conceitos/02-desenhando-objetos.py
+++ 0-conceitos/02-desenhando-objetos.py
@@ -2,6 +2,9 @@ import pygame
 from pygame.locals import *
 from sys import exit
 
+class Linha:
+    def __init__(self, tela, cor, posIniXY, posFimXY, larg):
+        pygame.draw.line(tela, cor, posIniXY, posFimXY, larg)
 
 pygame.init()
 
@@ -22,19 +25,30 @@ while True:
 
     ## Desenhando na tela
 
+    #propriedades do retângulo
     posRectX = 200
     posRectY = 300
     largRect = 40
     altRect = 50
     rgbRect = (255, 0, 0)
 
+    #propriedades do círculo
     posCircleX = 300
     posCircleY = 100
     raioCircle = 20
     rgbCircle = (0, 150, 255)
 
+    #propriedades da linha
+    rgbLinha = (255, 255, 0)
+    posLinhaInicioXY = (390, 10)
+    posLinhaFimXY = (390, 400)
+    larguraLinha = 5
 
     pygame.draw.rect( tela, rgbRect, (posRectX, posRectY, largRect, altRect))
     pygame.draw.circle(tela, rgbCircle, (posCircleX, posCircleY), raioCircle)
+    l1 = Linha(tela,rgbLinha,posLinhaInicioXY, posLinhaFimXY,larguraLinha)
+    l2 = Linha(tela,(0,255,100),(10,0),(10,500),20)
+    l3 = Linha(tela,(255,50,100),(30,0),(30,500),20)
+    l4 = Linha(tela,(15,255,255),(50,0),(50,500),20)
 
     pygame.display.update()
