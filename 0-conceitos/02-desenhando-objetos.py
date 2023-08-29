fa206f80d3cc09ac52f2c278904911694748a3cb
thiago-rocco <deroccotr@gmail.com>
2023-08-29 07:16:47 -0300

    Adicionando retangulo e circulo na tela

---
+++ 0-conceitos/02-desenhando-objetos.py
@@ -0,0 +1,38 @@
+import pygame
+from pygame.locals import *
+from sys import exit
+
+
+pygame.init()
+
+
+largura = 640
+altura = 480
+titulo = 'Meu Primeiro Jogo com Pygame'
+
+
+tela = pygame.display.set_mode((largura, altura))
+pygame.display.set_caption(titulo)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            exit()
+
+    ## Desenhando na tela
+
+    posRectX = 200
+    posRectY = 300
+    posCircleX = 300
+    posCircleY = 100
+    largRect = 40
+    altRect = 50
+    rgbRect = (255, 0, 0)
+    rgbCircle = (0,150,255)
+
+
+    pygame.draw.rect(tela, rgbRect, (posRectX, posRectY, largRect, altRect))
+    pygame.draw.circle(tela, rgbCircle, (posCircleX,posCircleY), 40)
+
+    pygame.display.update()
